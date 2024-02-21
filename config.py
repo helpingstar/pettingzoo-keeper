9e4bda2006aea32905225701f6abdaa12920c306
helpingstar <iamhelpingstar@gmail.com>
2024-02-21 20:49:58 +0900

    Update config

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ agent = {
     "n_agent_type": 5,
     "agent_type": [
         "normal",
-        "neg_on_all_state",
-        "pos_on_half",
-        "pos_on_quarter_down",
-        "pos_on_quarter_up",
+        "ne_all",
+        "po_half",
+        "po_qd",
+        "po_qu",
     ],
     "reward": [None, -0.001, 0.001, 0.001, 0.001],
 }
