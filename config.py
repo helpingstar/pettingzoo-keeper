8129f12a7c8c5895b5bb2013bacafc206f8205ff
helpingstar <iamhelpingstar@gmail.com>
2024-02-28 06:28:50 +0900

    Update config, black

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 experiment_name = "test"
 agent = {
-    "agent_type": [
-        "nml",  # normal
-        "nal",  # negative all
-        "phf",  # positive half
-    ],
+    "agent_type": ["nml", "nal", "phf"],  # normal, negative all, positive half
     "reward": [None, -0.001, 0.001],
 }
 selfplay = {"weight_divison": 2}
 wandb = {"silent": "true"}
-env = {"winning_score": 5}
+env = {"winning_score": 15}
 
 agent["n_agent_type"] = len(agent["agent_type"])
