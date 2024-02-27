fd43fd40c7b6d682a16e08d791487808c5c26da9
helpingstar <iamhelpingstar@gmail.com>
2024-02-27 10:50:27 +0900

    Update evaluate

--- evaluate.py
+++ evaluate.py
@@ -4,70 +4,128 @@ from pikazoo import pikazoo_v0
 from network import PPOAgent
 from gymnasium.experimental.wrappers import RecordVideoV0
 import supersuit as ss
-from pikazoo.wrappers import RecordEpisodeStatistics
 import utils
+import config
+
+from time import time
 
 
 class SharedEvaluater:
-    def __init__(self, experiment_name) -> None:
+    def __init__(self, experiment_name: str, dir_name: str) -> None:
+        assert torch.cuda.is_available(), "CUDA IS NOT AVAILABLE"
+        self.device = "cuda:0"
         self.experiment_name = experiment_name
 
-        self.rec_env = pikazoo_v0.env(render_mode="rgb_array")
-        self.rec_env = RecordVideoV0(self.rec_env)
-        self.rec_env = ss.dtype_v0(self.rec_env, np.float32)
-        self.rec_env = ss.normalize_obs_v0(self.rec_env)
+        all_weights = utils.get_all_agents_weights(self.experiment_name, True, False)
 
-        self.nml_env = pikazoo_v0.env(render_mode=None)
-        self.nml_env = ss.dtype_v0(self.nml_env, np.float32)
-        self.nml_env = ss.normalize_obs_v0(self.nml_env)
+        self.n_weight = len(all_weights["nml"])
 
-        self.weights = utils.get_all_agents_weights(self.experiment_name, True, False)
+        self.agents = {}
+        for agent_type, weights in all_weights.items():
+            self.agents[agent_type] = [
+                PPOAgent(f"Evaluater_{agent_type}").to(self.device).eval()
+                for _ in range(len(weights))
+            ]
 
-        self.agent1 = PPOAgent()
-        self.agent2 = PPOAgent()
+        for agent_type, agents in self.agents.items():
+            for i in range(len(agents)):
+                utils.load_weights(
+                    self.agents[agent_type][i], all_weights[agent_type][i]
+                )
 
-    def evaluate_two_agents(self, agent_type1, agent_type2):
-        pass
+        board_shape = (
+            config.agent["n_agent_type"],
+            config.agent["n_agent_type"],
+            self.n_weight,
+            self.n_weight,
+            2,
+        )
+
+        self.score_board = np.zeros(shape=board_shape)
+
+        self.winning_board = np.zeros(shape=board_shape)
+
+        self.agent_to_idx = {
+            agent: i for i, agent in enumerate(config.agent["agent_type"])
+        }
+
+    def evaluate_all(self):
+        print("[Evaulating Start]")
+        for i in range(config.agent["n_agent_type"]):
+            for j in range(config.agent["n_agent_type"]):
+                self.evaluate_two_agents(i, j)
+
+        for i in range(config.agent["n_agent_type"]):
+            for j in range(config.agent["n_agent_type"]):
+                print(self.winning_board[i][j])
+
+    def evaluate_two_agents(self, agent_idx1, agent_idx2):
+        agent_type1 = config.agent["agent_type"][agent_idx1]
+        agent_type2 = config.agent["agent_type"][agent_idx2]
+
+        print(f"  [Evaluate {agent_type1} VS {agent_type2}]")
+
+        for i in range(self.n_weight):
+            for j in range(self.n_weight):
+                print(f"    Fight {agent_type1}_{i} VS {agent_type2}_{j}")
+                self.fight(7, agent_type1, agent_type2, i, j, False)
 
     def fight(
-        self, n_episode: int, agent1: PPOAgent, agent2: PPOAgent, record_video: bool
+        self,
+        n_episode: int,
+        agent1_type: str,
+        agent2_type: str,
+        agent1_weight_idx,
+        agent2_weight_idx,
+        record_video: bool,
     ):
+        agent1 = self.agents[agent1_type][agent1_weight_idx]
+        agent2 = self.agents[agent2_type][agent2_weight_idx]
+
         assert not (
             agent1.training or agent2.training
         ), "The two networks should be in eval mode."
 
-        winning_count = np.array([0, 0])
-        cumulated_score = np.array([0, 0])
+        winning_count = self.winning_board[self.agent_to_idx[agent1_type]][
+            self.agent_to_idx[agent2_type]
+        ][agent1_weight_idx][agent2_weight_idx]
+        cumulated_score = self.score_board[self.agent_to_idx[agent1_type]][
+            self.agent_to_idx[agent2_type]
+        ][agent1_weight_idx][agent2_weight_idx]
 
         if record_video:
-            env = self.rec_env
+            env = pikazoo_v0.env(render_mode="rgb_array")
+            env = RecordVideoV0(env)
         else:
-            env = self.nml_env
-        agents = [agent1, agent2]
+            env = pikazoo_v0.env(render_mode=None)
+
+        env = ss.dtype_v0(env, np.float32)
+        env = ss.normalize_obs_v0(env)
+
+        agents_net = {env.possible_agents[0]: agent1, env.possible_agents[1]: agent2}
         with torch.inference_mode():
             for i in range(n_episode):
                 observations, infos = env.reset()
                 while env.agents:
                     # this is where you would insert your policy
                     actions = {
-                        agent: agents[i].get_action_and_value(observations)[0]
-                        for i, agent in enumerate(env.agents)
+                        agent: agents_net[agent].get_action_and_value(
+                            torch.tensor(observations[agent]).to(self.device)
+                        )[0]
+                        for agent in env.agents
                     }
                     observations, rewards, terminations, truncations, infos = env.step(
                         actions
                     )
-                if env.physics.player1.is_winner:
+                if infos["player_1"]["score"][0] > infos["player_1"]["score"][1]:
                     winning_count[0] += 1
                 else:
                     winning_count[1] += 1
-                cumulated_score[0] += env.scores[0]
-                cumulated_score[1] += env.scores[1]
+                cumulated_score[0] += infos["player_1"]["score"][0]
+                cumulated_score[1] += infos["player_1"]["score"][1]
         env.close()
-        p1_score_rate = cumulated_score[0] / cumulated_score.sum()
-        p1_winning_rate = winning_count[0] / winning_count.sum()
-
-        return p1_score_rate, p1_winning_rate
 
 
 if __name__ == "__main__":
-    evaluater = Evaluater("ex1")
+    evaluater = SharedEvaluater("ex1")
+    evaluater.evaluate_all()
