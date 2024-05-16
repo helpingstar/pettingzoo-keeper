a79e82b662558fbe60137f319c9df01dc4c4716d
helpingstar <iamhelpingstar@gmail.com>
2024-05-16 17:50:31 +0900

    Update

--- test_codes/test7_sandbox.py
+++ test_codes/test7_sandbox.py
@@ -2,28 +2,76 @@ from pikazoo import pikazoo_v0
 import supersuit as ss
 import numpy as np
 from pikazoo.wrappers import RecordEpisodeStatistics, NormalizeObservation
+import torch.nn as nn
+from torch.distributions.categorical import Categorical
+import torch
 
-num_envs = 8
+
+class Agent(nn.Module):
+    def __init__(self, envs):
+        super().__init__()
+        self.critic = nn.Sequential(
+            nn.Linear(35, 1),
+        )
+        self.actor = nn.Sequential(
+            nn.Linear(35, 18),
+        )
+
+    def get_value(self, x):
+        return self.critic(x)
+
+    def get_action_and_value(self, x, action=None):
+        logits = self.actor(x)
+        probs = Categorical(logits=logits)
+        if action is None:
+            action = probs.sample()
+        return action, probs.log_prob(action), probs.entropy(), self.critic(x)
+
+
+num_envs = 6
 
 env = pikazoo_v0.env(winning_score=2, render_mode=None)
-env = NormalizeObservation(env)
-env = RecordEpisodeStatistics(env)
+# env = NormalizeObservation(env)
+# env = RecordEpisodeStatistics(env)
 
 
 env = ss.pettingzoo_env_to_vec_env_v1(env)
-envs = ss.concat_vec_envs_v1(env, num_envs, num_cpus=8, base_class="gymnasium")
-observations, infos = envs.reset()
+envs = ss.concat_vec_envs_v1(env, num_envs, num_cpus=6, base_class="gymnasium")
 
-print(envs.observation_space())
-print(envs.action_space())
+print(envs.idx_starts)
+agent = Agent(envs)
 
-for i in range(100000):
-    actions = np.random.randint(0, 18, size=(num_envs * 2))
-    print(observations.shape)
-    observations, rewards, terminations, truncations, infos = envs.step(actions)
-    print("-----")
-    # print(infos)
-env.close()
+p1_indices = np.array(envs.idx_starts[:-1])
+p2_indices = p1_indices + 1
+
+
+def split_p(x):
+    return x[p1_indices], x[p2_indices]
+
+
+# print(p1_indices, p2_indices)
+
+observations_all, infos_all = envs.reset()
+
+print(observations_all.shape)
+action = agent.get_action_and_value(torch.Tensor(observations_all))[0]
+action_np = action.cpu().numpy()
+print()
+
+# print(split_p(observations_all))
+
+# envs.step()
+
+# print(envs.observation_space())
+# print(envs.action_space())
+
+# for i in range(100000):
+#     actions = np.random.randint(0, 18, size=(num_envs * 2))
+#     print(observations.shape)
+#     observations, rewards, terminations, truncations, infos = envs.step(actions)
+#     print("-----")
+#     # print(infos)
+# env.close()
 
 # observations, infos = env.reset()
 # for i in range(100000):
@@ -32,3 +80,6 @@ env.close()
 #     if not env.agents:
 #         observations, infos = env.reset()
 # env.close()
+
+
+envs.close()
--- utils.py
+++ utils.py
@@ -1,171 +1,27 @@
-import torch.nn as nn
 import numpy as np
-import os
-from pathlib import Path
-import config
-import torch
-from network import PPOAgent
-from typing import List, Dict
 
 
-def copy_network(src: nn.Module, dest: nn.Module):
-    dest.load_state_dict(src.state_dict())
-
-
-def create_files_in_leaf_folders(directory):
-    # 현재 디렉토리가 leaf 디렉토리인지 확인하는 플래그
-    is_leaf = True
-
-    # 현재 디렉토리 내의 모든 항목을 반복
-    for item in os.listdir(directory):
-        item_path = os.path.join(directory, item)
-        # 항목이 디렉토리인 경우 재귀 호출
-        if os.path.isdir(item_path):
-            is_leaf = False
-            create_files_in_leaf_folders(item_path)
-
-    # 현재 디렉토리가 leaf 디렉토리인 경우
-    if is_leaf:
-        # 1.txt, 2.txt, 3.txt, 4.txt 파일 생성
-        for i in range(1, 19):
-            file_path = Path(directory) / f"saved_{i:03d}.txt"
-            file_path.touch()
-        file_path = Path(directory) / f"main.txt"
-        file_path.touch()
-
-
-def init_dir(experiment_name):
-    # Create a experiment folder
-    os.makedirs(experiment_name)
-
-    for agent_type in config.agent["agent_type"]:
-        agent_folder_path = os.path.join(experiment_name, agent_type)
-        os.makedirs(agent_folder_path)
-        path_independent = os.path.join(agent_folder_path, "independent")
-        path_shared = os.path.join(agent_folder_path, "shared")
-        os.makedirs(path_independent)
-        os.makedirs(path_shared)
-
-
-def delete_files_in_leaf_folders(start_path):
-    # 현재 폴더가 leaf 폴더인지 확인하기 위해 하위 폴더 존재 여부를 체크합니다.
-    is_leaf = True
-    for item in os.listdir(start_path):
-        if os.path.isdir(os.path.join(start_path, item)):
-            # 하위 폴더가 있으면, 현재 폴더는 leaf 폴더가 아니며,
-            # 해당 하위 폴더에 대해 재귀적으로 함수를 호출합니다.
-            is_leaf = False
-            delete_files_in_leaf_folders(os.path.join(start_path, item))
-
-    # 현재 폴더가 leaf 폴더이면, 폴더 내 모든 파일을 삭제합니다.
-    if is_leaf:
-        for item in os.listdir(start_path):
-            item_path = os.path.join(start_path, item)
-            if os.path.isfile(item_path):
-                os.remove(item_path)
-                print(f"Deleted file: {item_path}")
-
-
-def get_all_agents_weights(experiment_name: str, is_shared: bool, is_main: bool) -> Dict[str, List[str]]:
-    """A function that classifies and returns weights for each agent type.
-
-    Args:
-        experiment_name (str): Name of experiment
-        is_shared (bool): shared or independent
-        is_main (bool): main or saved
-
-    Returns:
-        Dict[str, List[str]]: The key corresponds to the agent type, and the value stores a list of weight paths that meet the condition.
-    """
-    weights_list = dict()
-    for agent in config.agent["agent_type"]:
-        agent_weight = get_weights(experiment_name, agent, is_shared, is_main)
-        weights_list[agent] = agent_weight
-    return weights_list
-
-
-def get_all_saved_weights_list(experiment_name: str, is_shared: bool) -> List[str]:
-    """Stores all weights in a single list. They are sorted in ascending order by time.
-
-    Args:
-        experiment_name (str): name of experiment
-        is_shared (bool): shared or independent
-
-    Returns:
-        List[str]: List composed of Paths
-    """
-    result = []
-    temp = get_all_agents_weights(experiment_name, is_shared, False)
-    for li in temp.values():
-        result.extend(li)
-    result.sort(key=lambda x: (x[-7:], x))
-    return result
-
-
-def get_weights(experiment_name, agent_name, is_shared, is_main):
-    kind_of_train = "shared" if is_shared else "independent"
-    kind_of_weight = "main" if is_main else "saved"
-    path = os.path.join(experiment_name, agent_name, kind_of_train)
-    weight_list = []
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if kind_of_weight in file:
-                weight_list.append(os.path.join(root, file))
-    weight_list.sort()
-    return weight_list
-
-
-def get_schedule(n_agent, n_weights, n_divison, get_all_weights=False):
-    schedules = []
-    boundaries = []
-    if get_all_weights:
-        n_weights *= n_agent
-    diff = n_weights / n_divison
-    for i in range(n_divison):
-        boundaries.append(int(i * diff))
-    for i in range(n_agent):
-        weight_nums = [np.random.randint(d, n_weights) for d in boundaries]
-        schedules.append(weight_nums)
-
-    return schedules
-
-
-def get_schedule2(n_agent, n_weights):
-    n_weights *= n_agent
-    boundary = int(n_weights * 0.7)
-    result = []
-    for _ in range(n_agent):
-        coin = np.random.rand()
-        if coin < 0.7:
-            result.append(np.random.randint(boundary, n_weights))
+class ElementHandler:
+    def __init__(self, idx_starts: list[int], train_id) -> None:
+        self.num_envs = len(idx_starts)
+        if train_id == "p1":
+            self.indices_train = np.array(idx_starts)
+            self.indices_infer = self.indices_train + 1
         else:
-            result.append(np.random.randint(0, boundary))
-    return result
+            self.indices_infer = np.array(idx_starts)
+            self.indices_train = self.indices_infer + 1
 
+    def get_train(self, element):
+        return element[self.indices_train]
+    
+    def get_infer(self, element)
+        return element[self.indices_infer]
 
-def get_run_name(div_idx, agent_opp, agent_opp_idx, schedule):
-    return f"{div_idx * 5 + agent_opp_idx:03d}_{agent_opp}_{schedule[agent_opp_idx][div_idx]:03d}"
+    def split(self, element):
+        return element[self.indices_train], element[self.indices_infer]
 
-
-def get_info_by_path(path: str):
-    infos = path.split("/")
-    return infos[0], infos[1], infos[2], infos[3]
-
-
-def load_weights(dst: PPOAgent, src: str, tab: int = 0):
-    print(f"{' '*(2*tab)}{dst.name} <- {src}")
-    dst.load_state_dict(torch.load(src))
-
-
-def save_weights(net: PPOAgent, path: str, tab: int = 0):
-    print(f"{' '*(2*tab)}{net.name} -> {path}")
-    torch.save(net.state_dict(), path)
-
-
-if __name__ == "__main__":
-    # init_dir("ex2")
-    # create_files_in_leaf_folders("ex2")
-    # paths = get_all_saved_weights_list("ex2", True)
-    # print(paths)
-    # print(os.path.split(paths[0]))
-    print(get_schedule(5, 10, 3, True))
+    def combine(self, element_train, element_infer):
+        element = np.zeros(shape=(self.num_envs * 2,))
+        element[self.indices_train] = element_train
+        element[self.indices_infer] = element_infer
+        return element
